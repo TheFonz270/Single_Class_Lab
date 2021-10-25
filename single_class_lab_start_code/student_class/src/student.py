33d06bea57afeea8039abd29a1bb70ac5d298a57
Martin Quinn <thefonz270@hotmail.com>
2021-10-25 14:40:43 +0100

    student can talk

--- single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab_start_code/student_class/src/student.py
@@ -2,3 +2,12 @@ class Student:
     def __init__ (self, name, uid):
         self.name = name
         self.cohort = uid
+
+    def test_student_can_update_name(self, new_name):
+        self.name = new_name
+
+    def test_student_can_change_cohort(self, uid):
+        self.cohort = uid
+
+    def talk(self):
+        return "I can talk!"
