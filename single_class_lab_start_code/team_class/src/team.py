0ccfcc282b50952880023c3f4afc6aebc17d48eb
Martin Quinn <thefonz270@hotmail.com>
2021-10-25 15:01:41 +0100

    Teams Tests passed.

--- single_class_lab_start_code/team_class/src/team.py
+++ single_class_lab_start_code/team_class/src/team.py
@@ -3,6 +3,7 @@ class Team:
         self.name = name
         self.players = players
         self.coach = coach
+        self.points = 0 
     
     def add_player(self, new_player):
         self.players.append(new_player)
@@ -12,3 +13,9 @@ class Team:
             return True 
         else:
             return False
+    
+    def play_game(self, play_game):
+        if play_game == True:
+            self.points += 3
+        else:
+            self.points = 0
