5a74868d67501e808072cd904965030f06f4260d
Martin Quinn <thefonz270@hotmail.com>
2021-10-25 14:54:32 +0100

    First 5 tests passed

--- single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab_start_code/student_class/src/student.py
@@ -3,12 +3,6 @@ class Student:
         self.name = name
         self.cohort = uid
 
-    def test_student_can_update_name(self, new_name):
-        self.name = new_name
-
-    def test_student_can_change_cohort(self, uid):
-        self.cohort = uid
-
     def talk(self):
         return "I can talk!"
 
